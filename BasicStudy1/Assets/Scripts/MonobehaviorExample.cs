fb7fc06d59bc0fa31636f45b8a0d9eefe1e2ea4c
lsm8424 <lsm8424@naver.com>
2025-03-10 13:44:46 +0900

    d

---
+++ BasicStudy1/Assets/Scripts/MonobehaviorExample.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MonobehaviorExample : MonoBehaviour
+{
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        //
+    }
+}
---
+++ BasicStudy1/Assets/Scripts/VectorStudy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class VectorStudy : MonoBehaviour
+{
+    public Vector2 v2 = new Vector2(10, 20);
+    public Vector3 v3 = new Vector3(1, 1, 1);
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
